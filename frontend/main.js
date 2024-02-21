6360ae105ce1c5dba09ab1fe9d5f1aec9f9fc026
haroonwq <haroonwq@gmail.com>
2024-02-21 19:36:46 +0500

    re-doing the project - updates to structure - created main.js file

--- frontend/main.js
+++ frontend/main.js
@@ -5,15 +5,18 @@ window.addEventListener('DOMContentLoaded', (event) => {
 const functionApi = '';
 
 const getVisitCount = () => {
+
     let count = 30;
     fetch(functionApi).then(Response => {
         return Response.json()
-    }).then(response =>{
-        console.log("Website called function API.");
+
+    }).then(response => {
+        console.log("Website called function API");
         count = response.count;
         document.getElementById("counter").innerText = count;
     }).catch(function(error){
         console.log(error);
     });
     return count;
+
 }
